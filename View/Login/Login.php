c79c4c45da5a0768d0a94a79c51676af4b0015cf
jefmora <106130552+jefmora@users.noreply.github.com>
2025-03-10 18:47:28 -0600

    ajuste login

--- View/Login/Login.php
+++ View/Login/Login.php
@@ -1,84 +1,98 @@
 <?php
     include_once $_SERVER["DOCUMENT_ROOT"] . "/FerreSoluciones/Controller/LoginController.php";
     include_once $_SERVER["DOCUMENT_ROOT"] . "/FerreSoluciones/View/layoutInterno.php";
-    include_once $_SERVER["DOCUMENT_ROOT"] . "/FerreSoluciones/Model/BaseDatosModel.php";
+    
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 
-<?php
-function PrintCss() {
-    echo '<link href="path/to/your/css/file.css" rel="stylesheet">';
-}
-function PrintScript() {
-    echo '<script src="path/to/your/script/file.js"></script>';
-}
-PrintCss();
-?>
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Ferresoluciones</title>
+        <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
+        <link href="../Styles/styles.css" rel="stylesheet" />
+        <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+    </head>
+  
+    <body class="d-flex flex-column min-vh-100">
+        <div class="container MargenSuperior">
+            <div class="row justify-content-center">
+                <div class="col-xl-6 col-lg-12 col-md-9">
+                    <div class="card o-hidden border-9 shadow-lg my-5">
+                        <div class="card-body p-6">
+                            <div class="row">
+                                <div class="col-lg-3"></div>
+                                    <div class="col-lg-12 text-center">
+                                        <div class="p-5">
 
-<body class="bg-gradient-primary">
+                                            <div class="text-center">
+                                                <h1 class="h1 text-gray-900 mb-4">FerreSoluciones</h1>
+                                            </div>
 
-    <div class="container MargenSuperior">
+                                            <br>
+                                            
+                                            <?php
+                                                if(isset($_POST["Message"]))
+                                                {
+                                                echo '<div class="alert alert-warning Mensajes">' . $_POST["Message"] . '</div>';                                   
+                                                }
+                                            ?>
+                                            <form action="" method="POST" class="user">
+                                                <div class="form-group">
+                                                    <input type="text" class="form-control form-control-user"
+                                                        id="txtIdentificacion" name="txtIdentificacion"
+                                                        placeholder="Identificación" required>
+                                                </div>
+                                                <br>
+                                                <div class="form-group">
+                                                    <input type="password" class="form-control form-control-user"
+                                                        id="txtContrasenna" name="txtContrasenna" 
+                                                        placeholder="Contraseña" required>
+                                                        
+                                                </div>
+                                                <br>
+                                                <br>
+                                                <input type="submit" class="btn btn-warning btn-user btn-block" value="Procesar"
+                                                id="btnIniciarSesion" name="btnIniciarSesion" >
+                                            </form>
 
-        <div class="row justify-content-center">
+                                            <hr>
+                                        
+                                            <div class="text-center">
+                                                <a class="small" href="recuperarContrasenna.php">Recuperar Contraseña</a>
+                                            </div>
 
-            <div class="col-xl-10 col-lg-12 col-md-9">
+                                            <div class="text-center">
+                                                <a class="small" href="registrarCuenta.php">Crear una Cuenta</a>
+                                            </div>
 
-                <div class="card o-hidden border-0 shadow-lg my-5">
-                    <div class="card-body p-0">
-
-                        <div class="row">
-                            <div class="col-lg-3"></div>
-                            <div class="col-lg-6">
-                                <div class="p-5">
-                                    <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">FerreSoluciones</h1>
-                                    </div>
-
-                                    <?php
-                                        if(isset($_POST["Message"]))
-                                        {
-                                            echo '<div class="alert alert-warning Mensajes">' . $_POST["Message"] . '</div>';                                   
-                                        }
-                                    ?>
-                                    <form action="" method="POST" class="user">
-                                        <div class="form-group">
-                                            <input type="text" class="form-control form-control-user"
-                                                id="txtIdentificacion" name="txtIdentificacion"
-                                                placeholder="Identificación" required>
                                         </div>
-                                        <div class="form-group">
-                                            <input type="password" class="form-control form-control-user"
-                                                id="txtContrasenna" name="txtContrasenna" 
-                                                placeholder="Contraseña" required>
-                                        </div>
-                                        <input type="submit" class="btn btn-danger btn-user btn-block" value="Procesar"
-                                        id="btnIniciarSesion" name="btnIniciarSesion" >
-                                    </form>
-
-                                    <hr>
-                                    
-                                    <div class="text-center">
-                                        <a class="small" href="recuperarContrasenna.php">Recuperar Contraseña</a>
-                                    </div>
-                                    <div class="text-center">
-                                        <a class="small" href="registrarCuenta.php">Crear una Cuenta</a>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-
             </div>
+        
+      
 
-        </div>
-
-    </div>
-
-    <?php PrintScript(); ?>
-
-</body>
-
+        <footer class="py-4 bg-light mt-auto">
+            <div class="container-fluid px-4">
+                <div class="d-flex align-items-center justify-content-between small">
+                    <div class="text-muted">Copyright &copy; Your Website 2023</div>
+                       
+                </div>
+            </div>
+        </footer>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="../Scripts/scripts.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js" crossorigin="anonymous"></script>
+    </body>
 </html>
