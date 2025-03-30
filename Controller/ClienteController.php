77c1be3dc6080cf63f11acde871593e0d4df7c1c
SebasCA110 <147111929+SebasCA110@users.noreply.github.com>
2025-03-30 12:38:33 -0600

    Commit clientes

---
+++ Controller/ClienteController.php
@@ -0,0 +1,149 @@
+<?php
+    include_once $_SERVER["DOCUMENT_ROOT"] . '/FerreSoluciones/Model/LoginModel.php';
+    include_once $_SERVER["DOCUMENT_ROOT"] . '/FerreSoluciones/Model/ClienteModel.php';
+
+    if(session_status() == PHP_SESSION_NONE) {
+        session_start();
+    }
+
+    if(isset($_POST["btnActualizarAcceso"]))
+    {
+        $contrasenaActual = $_POST["txtContrasenaActual"];
+        $contrasenaNueva = $_POST["txtContrasenaNueva"];
+        $contrasenaConfirmar = $_POST["txtContrasenaConfirmar"];
+
+        if($contrasenaActual == $contrasenaNueva)
+        {
+            $_POST["txtMensaje"] = "Debe ingresar una contraseña nueva";
+        }
+        else if($contrasenaNueva != $contrasenaConfirmar)
+        {
+            $_POST["txtMensaje"] = "Las nuevas contraseñas no coinciden";
+        }
+        else
+        {
+            $resultado = ActualizarContrasenaModel($_SESSION["ClienteID"], $contrasenaNueva);
+        
+            if($resultado == true)
+            {
+                header('location: ../../View/Login/home.php');
+            }
+            else
+            {
+                $_POST["txtMensaje"] = "Su contraseña no se ha actualizado correctamente";
+            }
+        }
+    }
+
+    function ConsultarCliente($clienteID)
+    {
+        $resultado = ConsultarClienteModel($clienteID);
+
+        if($resultado != null && $resultado -> num_rows > 0)
+        {
+            return mysqli_fetch_array($resultado);
+        }
+        else
+        {
+            $_POST["txtMensaje"] = "Su información no se ha obtenido correctamente";
+            #header('location: ../../View/Login/home.php');
+        }
+
+    }
+
+    function ConsultarClientes()
+    {
+        return ConsultarClientesModel($_SESSION["ClienteID"]);
+    }
+
+    if(isset($_POST["btnActualizarPerfil"]))
+    {
+
+        $cedula = $_POST["txtCedula"];
+        $nombre = $_POST["txtNombre"];
+        $apellido1 = $_POST["txtApellido1"]; 
+        $apellido2 = $_POST["txtApellido2"]; 
+        $rolID = $_POST["txtRol"];
+        $provinciaID = $_POST["ddlProvincias"]; 
+        $otrasSenas = $_POST["txtOtrasSenas"];
+        $codigoPostal = $_POST["txtCodigoPostal"]; 
+        $correo = $_POST["txtCorreo"];
+        $telefono = $_POST["txtTelefono"];
+
+        $resultado = ActualizarPerfilModel($_SESSION["ClienteID"],$cedula,$nombre,$apellido1,$apellido2,$rolID,
+        $provinciaID,$otrasSenas,$codigoPostal,$correo,$telefono);
+        
+        if($resultado == true)
+        {
+            $_SESSION["NombreCliente"] = $nombre;
+            header('location: ../../View/Login/home.php');
+        }
+        else
+        {
+            $_POST["txtMensaje"] = "Su información no se ha actualizado correctamente";
+        }
+    }
+
+        if(isset($_POST["btnActualizarCliente"]))
+        {
+            $clienteID = $_POST["txtClienteID"];
+            $cedula = $_POST["txtCedula"];
+            $nombre = $_POST["txtNombre"];
+            $apellido1 = $_POST["txtApellido1"]; 
+            $apellido2 = $_POST["txtApellido2"]; 
+            $rolID = $_POST["ddlRoles"];
+            $provinciaID = $_POST["ddlProvincias"]; 
+            $otrasSenas = $_POST["txtOtrasSenas"];
+            $codigoPostal = $_POST["txtCodigoPostal"]; 
+            $correo = $_POST["txtCorreo"];
+            $telefono = $_POST["txtTelefono"];
+    
+            $resultado = ActualizarPerfilModel($clienteID, $cedula, $nombre, $apellido1, $apellido2, $rolID, 
+            $provinciaID, $otrasSenas, $codigoPostal, $correo, $telefono);
+            
+            if($resultado == true)
+            {
+                header('location: ../../View/Cliente/consultarClientes.php');
+            }
+            else
+            {
+                $_POST["txtMensaje"] = "No fue posible actualizar la información del cliente";
+            }
+        }
+
+        if(isset($_POST["btnCambiarEstadoCliente"]))
+        {
+            $clienteID = $_POST["txtClienteID"];
+    
+            $resultado = CambiarEstadoClienteModel($clienteID);
+            
+            if($resultado == true)
+            {
+                header('location: ../../View/Cliente/consultarClientes.php');
+            }
+            else
+            {
+                $_POST["txtMensaje"] = "No fue posible actualizar el estado del cliente";
+            }
+        }
+
+        function ConsultarRoles()
+        {
+            $resultado = ConsultarRolesModel();
+    
+            if($resultado != null && $resultado -> num_rows > 0)
+            {
+                return $resultado;
+            }
+        }
+
+        function ConsultarProvincias()
+        {
+            $resultado = ConsultarProvinciasModel();
+            
+            if ($resultado != null && $resultado->num_rows > 0) {
+                return $resultado;
+            }
+        }
+    
+    ?>
---
+++ View/Cliente/consultarPerfil.php
@@ -0,0 +1,146 @@
+<?php
+    include_once $_SERVER["DOCUMENT_ROOT"] . '/FerreSoluciones/View/layout.php';
+    include_once $_SERVER["DOCUMENT_ROOT"] . '/FerreSoluciones/Controller/ClienteController.php';
+
+    $id = $_SESSION["ClienteID"];
+    $datos = ConsultarCliente($id);
+
+?>
+
+<!doctype html>
+<html lang="en">
+
+<?php
+    ReferenciasCSS();
+?>
+
+<body class="page-wrapper radial-gradient">
+    <div id="main-wrapper" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full"
+        data-sidebar-position="fixed" data-header-position="fixed" 
+
+        <?php
+            MostrarMenu();
+        ?>
+
+        <div class="body-wrapper">
+            
+        <?php
+                MostrarHeader();
+        ?> 
+            <div class="container-fluid">
+                <div class="row">
+
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title fw-semibold mb-4">Mi Perfil</h5>
+
+                            <?php
+                                if(isset($_POST["txtMensaje"]))
+                                {
+                                    echo '<div class="alert alert-info Centrado">' . $_POST["txtMensaje"] . '</div>';
+                                }
+                            ?>
+
+                            <form action="" method="POST">
+
+                                <div class="mb-3">
+                                    <label class="form-label">Cédula</label>
+                                    <input type="text" class="form-control" id="txtCedula"name="txtCedula" 
+                                    value="<?php echo $datos["cedula"] ?>">
+                                </div>
+
+                                <div class="mb-3">
+                                    <label class="form-label">Nombre</label>
+                                    <input type="text" class="form-control" id="txtNombre" name="txtNombre"
+                                    value="<?php echo $datos["nombre"] ?>">
+                                </div>
+
+                                <div class="mb-3">
+                                    <label class="form-label">Primer Apellido</label>
+                                    <input type="text" class="form-control" id="txtApellido1" name="txtApellido1"
+                                    value="<?php echo $datos["apellido1"] ?>">
+                                </div>
+
+                                <div class="mb-3">
+                                    <label class="form-label">Segundo Apellido</label>
+                                    <input type="text" class="form-control" id="txtApellido2" name="txtApellido2"
+                                    value="<?php echo $datos["apellido2"] ?>">
+                                </div>
+
+                                <div class="mb-4">
+                                    <label class="form-label">Rol</label>
+                                    <input type="text" class="form-control" id="txtRol" name="txtRol" readOnly="true"
+                                    style="background-color: #B0C4DE"
+                                    value="<?php echo $datos["nombreRol"] ?>">
+                                </div>
+
+                                <div class="mb-4">
+                                    <label class="form-label">Fecha de Registro</label>
+                                    <input type="text" class="form-control" id="txtFechaRegistro" name="txtFechaRegistro" readOnly="true"
+                                    style="background-color: #B0C4DE"
+                                    value="<?php echo $datos["fechaRegistro"] ?>">
+                                </div>
+
+                                <div class="mb-4">
+                                    <label class="form-label">Provincia</label>
+                                    <select id="ddlProvincias" name="ddlProvincias" class="form-control">
+                                        <?php
+                                            $provincias = ConsultarProvincias();
+                                            echo "<option value=''> Seleccione </option>";
+                                            while($fila = mysqli_fetch_array($provincias)) 
+                                            {
+                                                if($fila["provinciaID"] == $datos["provinciaID"]) 
+                                                {
+                                                    echo "<option selected value=" . $fila["provinciaID"] . ">" . $fila["provincia"] . "</option>";
+                                                } 
+                                                else 
+                                                {
+                                                    echo "<option value=" . $fila["provinciaID"] . ">" . $fila["provincia"] . "</option>";
+                                                }
+                                            }
+                                        ?>
+                                    </select>
+                                </div>
+
+                                <div class="mb-4">
+                                    <label class="form-label">Otras Señas</label>
+                                    <input type="text" class="form-control" id="txtOtrasSenas" name="txtOtrasSenas"
+                                    value="<?php echo $datos["otrasSenas"] ?>">
+                                </div>
+
+                                <div class="mb-4">
+                                    <label class="form-label">Código Postal</label>
+                                    <input type="text" class="form-control" id="txtCodigoPostal" name="txtCodigoPostal"
+                                    value="<?php echo $datos["codigoPostal"] ?>">
+                                </div>
+
+                                <div class="mb-3">
+                                    <label class="form-label">Correo Electrónico</label>
+                                    <input type="email" class="form-control" id="txtCorreo" name="txtCorreo"
+                                    value="<?php echo $datos["correo"] ?>">
+                                </div>
+
+                                <div class="mb-4">
+                                    <label class="form-label">Número de Teléfono</label>
+                                    <input type="text" class="form-control" id="txtTelefono" name="txtTelefono"
+                                    value="<?php echo $datos["telefono"] ?>">
+                                </div>
+
+                                <input type="submit" class="btn btn-primary" value="Procesar" id="btnActualizarPerfil"
+                                    name="btnActualizarPerfil">
+
+                            </form>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php
+        ReferenciasJS();
+    ?>
+
+</body>
+
+</html>
